5cd82b278b339fe57af0cb32a45b43194e362dd9
wanghaoyu001 <46880572+wanghaoyu001@users.noreply.github.com>
2019-10-08 14:41:21 +0800

    学生信息管理系统
    java面向对象项目

---
+++ StudentManagement/src/com/action/StudentAction.java
@@ -0,0 +1,10 @@
+package com.action;
+
+public class StudentAction {
+
+	public static void main(String[] args) {
+		// TODO 自动生成的方法存根
+
+	}
+
+}
---
+++ StudentManagement/src/com/bean/Student.java
@@ -0,0 +1,6 @@
+package com.bean;
+
+public class Student {
+	//1.存储容器声明
+    //2.容器操作(增删改查)
+}
